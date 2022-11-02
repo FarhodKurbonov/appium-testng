0dcd321e04d1cb73c47eadef3669ea44bd2c97a6
Farkhod Kurbonov <farhodjon.uz@gmail.com>
2022-11-02 12:17:23 +0500

    Mobile Automation | Lesson 10 | Set values into textboxes and complete login form

--- src/test/apilearning/ClickOnElement.java
+++ src/test/apilearning/ClickOnElement.java
@@ -1,16 +1,11 @@
-package apilearning;
+package test.apilearning;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import utils.AppiumDriverExtensions;
 
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-public class clickOnElement {
+public class ClickOnElement {
     public static void main(String[] args) {
             AppiumDriver appiumDriver = AppiumDriverExtensions.getAppiumDriver();
             WebElement loginLabel = appiumDriver.findElement(AppiumBy.accessibilityId("Login"));
